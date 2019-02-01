d1f3411b1c0f45537714655e6133fc10504ff6a8
lmaia-22 <luismsm14@gmail.com>
2019-02-01 18:03:17 +0000

    smtp

--- app.js
+++ app.js
@@ -1,10 +1,99 @@
-const http = require('http');
-const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+const transporter = nodemailer.createTransport({
+
+  host: 'smtp.gmail.com',
+  provider: 'gmail',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'unipessoal.cliente@gmail.com', // Enter here email address from which you want to send emails
+    pass: 'unipessoal56' // Enter here password for email account from which you want to send emails
+  },
+  tls: {
+  rejectUnauthorized: false
+  }
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin");
+  next();
+});
+
+app.post('/send', function (req, res) {
+
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+
+  let senderName = req.body.contactFormName;
+  let senderEmail = req.body.contactFormEmail;
+  let messageSubject = req.body.contactFormSubjects;
+  let messageText = req.body.contactFormMessage;
+  let copyToSender = req.body.contactFormCopy;
+
+  let mailOptions = {
+    to: ['luismsm14@gmail.com'], // Enter here the email address on which you want to send emails from your customers
+    from: senderName,
+    subject: messageSubject,
+    text: "Email cliente: " + senderEmail + 
+    "\nMensagem:" + messageText,
+    replyTo: senderEmail
+  };
+
+  if (senderName === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (senderEmail === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (messageSubject === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (messageText === '') {
+    res.status(400);
+    res.send({
+    message: 'Bad request'
+    });
+    return;
+  }
+
+  if (copyToSender) {
+    mailOptions.to.push(senderEmail);
+  }
+
+  transporter.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error);
+      res.end('error');
+    } else {
+      console.log('Message sent: ', response);
+      res.end('sent');
+    }
+  });
+});
+
+app.listen(port, function () {
+  console.log('Express started on port: ', port);
 });
